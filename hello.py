5aacf2b23bfa13871f554b3daa946b519249cca2
Muktha nayak <muktha.nayak2004@gmail.com>
2026-07-15 16:09:55 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-# hello.py
-name = input("Enter your name: ")
-print("Hello,", name)
-
-# simple add
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-print("Sum:", a + b)
