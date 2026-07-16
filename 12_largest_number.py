a808e21029982371f060ae4c4a7c4686b308f6b5
Muktha nayak <muktha.nayak2004@gmail.com>
2026-07-17 01:46:53 +0530

    Add Python programs 11-20

---
+++ 12_largest_number.py
@@ -0,0 +1,5 @@
+numbers = [10, 50, 20, 80, 40]
+
+largest = max(numbers)
+
+print("Largest number =", largest)
---
+++ 13_smallest_number.py
@@ -0,0 +1,5 @@
+numbers = [10, 50, 20, 80, 40]
+
+smallest = min(numbers)
+
+print("Smallest number =", smallest)
